67a930168f415c90221ae60fdd46ab4715167e96
orestispanago <orestispanago@gmail.com>
2021-10-13 19:01:23 +0300

    Saving visible, thermal and temperature data. Added python plot script

--- src/CapPicture.cpp
+++ src/CapPicture.cpp
@@ -32,10 +32,6 @@ int Demo_Capture()
         return HPR_ERROR;
     }
 
-    //
-    // NET_DVR_JPEGPARA strPicPara = {0};
-    // strPicPara.wPicQuality = 2;
-    // strPicPara.wPicSize = 0;
 
     NET_DVR_JPEGPICTURE_WITH_APPENDDATA data = {0};
     data.pJpegPicBuff = new char[BUF_LEN];
@@ -53,11 +49,15 @@ int Demo_Capture()
         return HPR_ERROR;
     }
 
+    // printf("pyd---Channel %d dwP2PDataLen is %u.\n", devChnl, data.dwP2PDataLen);
+    // printf("pyd---Channel %d dwJpegPicWidth is %u.\n", devChnl, data.dwJpegPicWidth);
+    // printf("pyd---Channel %d dwJpegPicHeight is %u.\n", devChnl, data.dwJpegPicHeight);
+
     char fname[1024];
     struct tm *timenow;
     time_t now = time(NULL);
     timenow = gmtime(&now);
-    strftime(fname, sizeof(fname), "%Y%m%d_%H%M%S.dat", timenow);
+    strftime(fname, sizeof(fname), "%Y%m%d_%H%M%S", timenow);
 
     auto dump2file = [](std::string fname, char *d, int dsize)
     {
@@ -65,25 +65,14 @@ int Demo_Capture()
         f.write((char *)d, dsize);
         f.close();
     };
+    std::string s;
+    s+=fname;
 
-    dump2file(fname, data.pP2PDataBuff, data.dwP2PDataLen);
-    // dump2file(fname, data.pJpegPicBuff, data.dwJpegPicLen);
-    // dump2file(fname, data.pVisiblePicBuff, data.dwVisiblePicLen);
-    int iReta;
+    dump2file(s+".dat", data.pP2PDataBuff, data.dwP2PDataLen);
+    dump2file(s+"_t.jpeg", data.pJpegPicBuff, data.dwJpegPicLen);
+    dump2file(s+"_v.jpeg", data.pVisiblePicBuff, data.dwVisiblePicLen);
 
-    //1.Get picture params.
-    DWORD uiReturnLen;
-    NET_DVR_PICCFG_V30 struParams = {0};
-    struParams.dwSize = sizeof(NET_DVR_PICCFG_V30);
-    iReta = NET_DVR_GetDVRConfig(lUserID, NET_DVR_GET_PICCFG_V30, devChnl, &struParams, sizeof(NET_DVR_PICCFG_V30), &uiReturnLen);
-    if (!iReta)
-    {
-        printf("pyd---NET_DVR_GET_PICCFG _V30 %d error.\n", NET_DVR_GetLastError());
-        return HPR_ERROR;
-    }
-    printf("pyd---Channel %d dwP2PDataLen is %u.\n", devChnl, data.dwP2PDataLen);
-
-    //logout
+    // logout
     NET_DVR_Logout_V30(lUserID);
     NET_DVR_Cleanup();
 
