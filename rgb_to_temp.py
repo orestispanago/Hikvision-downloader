ddc368d85bd04e1b43f2138c10a6bd519359c93e
orestispanago <orestispanago@gmail.com>
2021-10-14 03:51:10 +0300

    Downloading highest resolution image using wget

--- rgb_to_temp.py
+++ rgb_to_temp.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-min_temp = 20.9
-max_temp = 23.3
+min_temp = 22.9
+max_temp = 20.5
 
-fname = "150.140.194.27_02_20211014001354584.jpeg"
+fname = "picture.jpeg"
 
 im = Image.open(fname)
 rgb = np.array(im.getdata())
@@ -21,7 +21,7 @@ intercept = linregress.intercept
 
 grayscale_converted = slope * grayscale + intercept
 
-arr = np.reshape(grayscale_converted, [240, 320])
+arr = np.reshape(grayscale_converted, [720, 1280])
 
 plt.imshow(arr, interpolation='none')
 plt.colorbar(label="Temperature °C")
